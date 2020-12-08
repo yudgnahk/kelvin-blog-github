d7c049a5a338acf543a584733a2cdcff2424274e
khanghldk <haleduykhang@gmail.com>
2020-12-08 12:01:44 +0000

    Deploying to gh-pages from  @ cc062a9909c9a2a23866bb0830d6dd725facada1 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-blog-github/precache-manifest.27ef8959cc1ea461bc9d168b2ea2d2ba.js"
+  "/kelvin-blog-github/precache-manifest.e8f25edad7ea451d698a7bee002b7c3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blog-github/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kelvin-blog-github/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
