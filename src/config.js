c7c127311b14d942998d7d8ca41375cf88990792
Khang Ha <v.khanghld@vinid.net>
2020-12-08 18:22:49 +0700

    Initialize

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "MTRjYWU5ZjRkOTQyYjQ4ZTg5NzNiOWE0MjY1OWQ2OWE1ZTg2YzE0ZA==",
+
+  // Your Github UserName
+  githubUserName: "khanghldk",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "kelvin-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.d40dedc23644e2a3',
+  OAuthSecret: '7052cafcef7e552558814a47a37ab5ecb4b64f99',
+
+  // Your Personal Blog Title
+  title : "Kelvin" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Engineer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
